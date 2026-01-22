e2e4738e9670da330caeb2e1c268c8c750087963
CJ Wang <lejacp@gmail.com>
2026-01-22 11:31:00 +0800

    add banner and hero in homepage

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from '../components/home/Banner'
+import Hero from '../components/home/Hero'
 
 const Home = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>
+                <Banner />
+                <Hero />
+            </h1>
         </div>
     )
 }
