1e11ebbb32183e76446c6ecfb5b6556711f1b846
CJ Wang <lejacp@gmail.com>
2026-01-22 14:35:16 +0800

    add Features.jsx to home

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Banner from '../components/home/Banner'
-import Hero from '../components/home/Hero'
+import React from "react";
+import Banner from "../components/home/Banner";
+import Hero from "../components/home/Hero";
+import Features from "../components/home/Features";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>
-                <Banner />
-                <Hero />
-            </h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>
+        <Banner />
+        <Hero />
+        <Features />
+      </h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
