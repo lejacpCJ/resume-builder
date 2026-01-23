4a3ec67c4613240a64121b68f425cb8b3ffce905
CJ Wang <lejacp@gmail.com>
2026-01-23 12:18:18 +0800

    add dummy data

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-import { PlusIcon, UploadCloudIcon } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import {
+  FilePenLineIcon,
+  PencilIcon,
+  PlusIcon,
+  TrashIcon,
+  UploadCloudIcon,
+} from "lucide-react";
+import { dummyResumeData } from "../assets/assets";
 
 const Dashboard = () => {
+  const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"];
+  const [allResumes, setAllResumes] = useState([]);
+  const loadAllResumes = async () => {
+    setAllResumes(dummyResumeData);
+  };
+  useEffect(() => {
+    loadAllResumes();
+  }, []);
+
   return (
     <div>
       <div className="max-w-7xl mx-auto px-4 py-8">
@@ -23,6 +39,42 @@ const Dashboard = () => {
           </button>
         </div>
         <hr className="border-slate-300 my-6 sm:w-[305px]" />
+        <div className="grid grid-cols-2 sm:flex flex-wrap gap-4">
+          {allResumes.map((resume, index) => {
+            const baseColor = colors[index % colors.length];
+            return (
+              <button
+                key={index}
+                className="relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer"
+                style={{
+                  background: `linear-gradient(135ddeg, ${baseColor}10, ${baseColor}40)`,
+                  borderColor: baseColor + "40",
+                }}
+              >
+                <FilePenLineIcon
+                  className="size-7 group-hover:scale-105 transition-all"
+                  style={{ color: baseColor }}
+                />
+                <p
+                  className="text-sm group-hover:scale-105 transition-all px-2 text-center"
+                  style={{ color: baseColor }}
+                >
+                  {resume.title}
+                </p>
+                <p
+                  className="absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all duration-300 px-2 text-center"
+                  style={{ color: baseColor + "90" }}
+                >
+                  Updated on {new Date(resume.updatedAt).toLocaleDateString()}
+                </p>
+                <div className="absolute top-1 right-1 group-hover:flex items-center hidden">
+                  <TrashIcon className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors" />
+                  <PencilIcon className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors" />
+                </div>
+              </button>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
