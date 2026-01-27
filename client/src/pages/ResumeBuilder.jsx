8cc3a30e764c398ced7722d2f8adb7981340f264
CJ Wang <lejacp@gmail.com>
2026-01-27 17:21:13 +0800

    add skills form

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -21,6 +21,7 @@ import ProfessionalSummary from "../components/ProfessionalSummary";
 import ExperienceForm from "../components/ExperienceForm";
 import EducationForm from "../components/EducationForm";
 import ProjectForm from "../components/ProjectForm";
+import SkillsForm from "../components/SkillsForm";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -186,6 +187,15 @@ const ResumeBuilder = () => {
                     data={resumeData.project}
                     onChange={(data) =>
                       setResumeData((prev) => ({ ...prev, project: data }))
+                    }
+                  />
+                )}
+
+                {activeSection.id === "skills" && (
+                  <SkillsForm
+                    data={resumeData.skills}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({ ...prev, skills: data }))
                     }
                   />
                 )}
