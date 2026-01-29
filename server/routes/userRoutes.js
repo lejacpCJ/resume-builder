a8daeec1537dc5fa0d46645189b63776135bb624
CJ Wang <lejacp@gmail.com>
2026-01-29 22:15:46 +0800

    add resume schema and get it by controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,3 +8,6 @@ const userRouter = express.Router();
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/data', protect, getUserById);
+userRouter.get('/resumes', protect, getUserResumes);
+
+export default userRouter;
