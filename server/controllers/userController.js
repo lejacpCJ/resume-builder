17487b425a1545851b223c5b6ebd489cc87af38a
CJ Wang <lejacp@gmail.com>
2026-01-29 16:34:08 +0800

    implement get user id

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -70,3 +70,21 @@ export const loginUser = async (req, res) => {
 
 // Controller for getting user by id
 // GET: /api/users/data
+
+export const getUserById = async (req, res) => {
+    try {
+        const userId = req.userId;
+
+        // check if user exists
+        const user = await User.findById(userId);
+        if(!user) {
+            return res.status(404).json({message: 'User Not Found'})
+        }
+
+        // return user
+        user.password = undefined;
+        return res.status(200).json({user});
+    } catch (error) {
+        return res.status(400).json({message: error.message});
+    }
+}
