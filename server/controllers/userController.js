759c075fcb5b4ca72a099808e1f78de1d3f940bb
CJ Wang <lejacp@gmail.com>
2026-01-29 15:55:11 +0800

    implement user registration

---
+++ server/controllers/userController.js
@@ -0,0 +1,44 @@
+import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+
+const generateToken = (userId) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '7d'});
+    return token;    
+}
+
+// Controller for user registration
+// POST: /api/users/register
+export const registerUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        // check if required fields are present
+        if (!name || !email ||!password) {
+            return res.status(400).json({message: 'Missing required fields'})    
+        }
+
+        // check if user alreafy exists
+        const user = await User.findOne({email});
+        if (user) {
+            return res.status(400).json({message: 'User already exists'})
+        }
+
+        // create new user
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({
+            name, email, password: hashedPassword
+        });
+
+        // return success message
+        const token = generateToken(newUser._id);
+        newUser.password = undefined;
+
+        return res.status(201).json({message: "User created successfully", token, user: newUser});
+    } catch (error) {
+        return res.status(400).json({message: error.message});
+    }
+} 
+
+
+// Controller for user login
