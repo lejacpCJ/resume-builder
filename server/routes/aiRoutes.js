872be42d6178e1629b914c47cf0c50109137b4cd
CJ Wang <lejacp@gmail.com>
2026-01-30 16:05:32 +0800

    fix import typo

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import protect from "../middlewares/authMiddleware.js";
-import {enhanceJobDescription, enhanceProfessionalSummary, updateResume} from "../controllers/aiController.js"
+import {enhanceJobDescription, enhanceProfessionalSummary, uploadResume} from "../controllers/aiController.js"
 
 const aiRouter = express.Router();
 
 aiRouter.post('/enhance-pro-sum', protect, enhanceProfessionalSummary);
 aiRouter.post('/enhance-job-desc', protect, enhanceJobDescription);
-aiRouter.post('/update-reseum', protect, updateResume);
+aiRouter.post('/update-reseum', protect, uploadResume);
 
 export default aiRouter;
 
