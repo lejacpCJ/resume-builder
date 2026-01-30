b7d8b43ed62362c3bc8ff2b1f38adefdb188b0cf
CJ Wang <lejacp@gmail.com>
2026-01-30 17:28:04 +0800

    add auth login to pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Layout from "./pages/layout";
@@ -6,8 +6,37 @@ import Dashboard from "./pages/Dashboard";
 import ResumeBuilder from "./pages/ResumeBuilder";
 import Preview from "./pages/preview";
 import Login from "./pages/login";
+import { useDispatch } from "react-redux";
+import api from "./configs/api";
+import { setLoading } from "./app/features/authSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const getUserData = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      if (token) {
+        const { data } = await api.get("/api/users/data", {
+          headers: { Authorization: token },
+        });
+        if (data.user) {
+          dispatch(login({ token, user: data.user }));
+        }
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    } catch (error) {
+      dispatch(setLoading(false));
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <>
       <Routes>
@@ -19,7 +48,6 @@ const App = () => {
         </Route>
 
         <Route path="view/:resumeId" element={<Preview />} />
-        <Route path="login" element={<Login />} />
       </Routes>
     </>
   );
