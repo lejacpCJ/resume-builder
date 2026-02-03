091aad770c6bd4d87a3b700b57de2190013bc0ab
CJ Wang <lejacp@gmail.com>
2026-02-03 15:19:22 +0800

    fix caption

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { useSelector } from "react-redux";
 import Loader from "../components/Loader";
-import Login from "./login";
+import Login from "./Login";
 
 const Layout = () => {
   const { user, loading } = useSelector((state) => state.auth);
